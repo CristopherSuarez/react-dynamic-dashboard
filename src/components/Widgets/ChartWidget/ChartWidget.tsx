9f1c8acf99020c380f2a260f6882d1db86a0d39d
Cristopher Suarez <cristopher.suarez@jalasoft.com>
2025-10-29 13:09:47 -0400

    feat(widgets): render widget improvements & queries implementation

--- src/components/Widgets/ChartWidget/ChartWidget.tsx
+++ src/components/Widgets/ChartWidget/ChartWidget.tsx
@@ -2,15 +2,36 @@ import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
 import { Box, Stack } from '@mui/material';
 import { BarChart } from '@mui/x-charts';
 
+import EmptyPlaceHolder from '../../EmptyPlaceHolder/EmptyPlaceHolder';
+
 interface ChartWidgetProps {
   label: string;
   color?: string;
   iconColor?: string;
   direction?: 'horizontal' | 'vertical';
+  data?: Array<Record<string, string | []>>
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function parseFakerBarChartData(data: any[]) {
+  if (!data.length) return { xAxis: [], series: [] };
+  const xAxis = data.map(item => item.group);
+  const numericKeys = Object.keys(data[0]).filter(
+    key => key !== 'group' && typeof data[0][key] === 'number'
+  );
+  const series = numericKeys.map(key => ({
+    label: key,
+    data: data.map(item => item[key])
+  }));
+
+  return { xAxis, series };
+}
+
 
 // BarChart component
 function ChartWidget(props: ChartWidgetProps) {
+  console.log(props.data);
+  const { xAxis, series } = parseFakerBarChartData(props.data ?? []);
   return (
     <Box>
       <Stack spacing={2}>
@@ -23,12 +44,16 @@ function ChartWidget(props: ChartWidgetProps) {
           <BarChartOutlinedIcon style={{ color: props.iconColor ?? 'black' }} />
           {props.label}
         </Box>
-        <BarChart
-          xAxis={[{ data: ['group A', 'group B', 'group C'] }]}
-          series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
-          height={300}
-          layout={props.direction ?? 'vertical'}
-        />
+        {
+          props.data?.length === 0
+            ? <EmptyPlaceHolder />
+            : <BarChart
+                xAxis={[{ data: xAxis }]}
+                series={series}
+                height={300}
+                layout={props.direction ?? 'vertical'}
+              />
+        }
       </Stack>
     </Box>
   );
--- src/components/Widgets/NumberWidget/NumberWidget.tsx
+++ src/components/Widgets/NumberWidget/NumberWidget.tsx
@@ -2,35 +2,41 @@ import NumbersOutlinedIcon from '@mui/icons-material/NumbersOutlined';
 import { Box, Stack, Typography } from '@mui/material';
 
 interface NumberWidgetProps {
-  numberValue: number;
   label: string;
   color?: string;
   iconColor?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: Array<Record<string, any>>
 }
 
 function NumberWidget(props: NumberWidgetProps) {
   return (
-    <Box>
-      <Stack spacing={2}>
-        <Box
-          color="gray"
-          display="flex"
-          alignItems="center"
-          gap={1}
-        >
-          <NumbersOutlinedIcon
-            style={{ color: props.iconColor ?? 'black' }}
-          />
-          {props.label}
-        </Box>
+    <Stack spacing={2} height="100%">
+      <Box
+        color="gray"
+        display="flex"
+        alignItems="center"
+        gap={1}
+      >
+        <NumbersOutlinedIcon
+          style={{ color: props.iconColor ?? 'black' }}
+        />
+        {props.label}
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height="100%"
+      >
         <Typography
           color={props.color ?? 'black'}
-          variant='h4'
+          variant="h2"
         >
-          {props.numberValue}
+          {props.data.length}
         </Typography>
-      </Stack>
-    </Box>
+      </Box>
+    </Stack>
   );
 }
 
